6b716604269cc4a55c7a9e2b4e872614ff2367e9
Simon Dudley <simon.l.dudley@hotmail.com>
2019-07-08 21:34:55 +0100

    Create dummy skeleton game: shuffle, deal, flop, turn, river. No betting or winner.

--- src/main/java/com/simondudley/poker/Application.java
+++ src/main/java/com/simondudley/poker/Application.java
@@ -1,18 +1,10 @@
 package com.simondudley.poker;
 
-import org.checkerframework.common.value.qual.IntRange;
-
-import java.util.Optional;
-import java.util.function.IntSupplier;
-import java.util.stream.IntStream;
-
 final class Application {
 
     public static void main(String[] args) {
 
-        // Game
-        // Dealer
-        // Player
-        // Round - 2x, flop, turn, river
+        Game game = new Game();
+        game.startGame(3);
     }
 }
---
+++ src/main/java/com/simondudley/poker/Game.java
@@ -0,0 +1,62 @@
+package com.simondudley.poker;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Game {
+
+    public void startGame(int numPlayers) {
+
+        Deck deck = createAndShuffleDeck();
+        List<Player> players = createPlayers(numPlayers);
+        dealToPlayers(deck, players);
+
+        Set<Deck.Card> flop = deck.remove(3);
+        System.out.println("FLOP: " + flop);
+        Set<Deck.Card> turn = deck.remove(1);
+        System.out.println("TURN: " + turn);
+        Set<Deck.Card> river = deck.remove(1);
+        System.out.println("RIVER: " + river);
+    }
+
+    private void dealToPlayers(Deck deck, List<Player> players) {
+        for (Player player : players) {
+            Set<Deck.Card> hand = deck.remove(2);
+            player.hand = hand;
+        }
+    }
+
+    private Deck createAndShuffleDeck() {
+        Deck deck = Deck.newInstance();
+        System.out.println(deck);
+        System.out.println("SHUFFLE");
+        deck.shuffle();
+        System.out.println(deck);
+        return deck;
+    }
+
+    private List<Player> createPlayers(int n) {
+        return IntStream.rangeClosed(1, n).mapToObj(Player::new).collect(toList());
+    }
+
+    public static class Player {
+
+        int id;
+        Set<Deck.Card> hand;
+
+        Player(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public String toString() {
+            return "Player{" +
+                    "id=" + id +
+                    ", hand=" + hand +
+                    '}';
+        }
+    }
+}

b4fbc5e1df479deb0ee43dc1a99cb58c2518a5b7
Simon Dudley <simon.l.dudley@hotmail.com>
2019-07-13 23:08:32 +0100

    Tidy up and extract methods for clarity

--- src/main/java/com/simondudley/poker/Hand.java
+++ src/main/java/com/simondudley/poker/Hand.java
@@ -3,12 +3,11 @@ package com.simondudley.poker;
 import com.google.common.collect.Lists;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.toList;
-import static com.simondudley.poker.Hand.HandValue.*;
+
 import static com.simondudley.poker.Card.Rank;
 import static com.simondudley.poker.Card.Suit;
+import static com.simondudley.poker.Hand.HandValue.*;
+import static java.util.stream.Collectors.*;
 
 class Hand implements Comparable<Hand> {
 
@@ -37,7 +36,6 @@ class Hand implements Comparable<Hand> {
         static final Comparator<HandValue> HAND_VALUE_COMPARATOR = Comparator.comparingInt(HandValue::getValue);
     }
 
-
     private static final int HAND_SIZE = 5;
     private final HandValue handValue;
     private final List<Card> cards;
@@ -56,7 +54,7 @@ class Hand implements Comparable<Hand> {
 
         // of a kind...
         Map<Rank, List<Card>> byRank = hand.stream().collect(groupingBy(c -> c.rank));
-        Map<Rank, Integer> sizeByRank = byRank.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().size()));
+        Map<Rank, Integer> sizeByRank = byRank.entrySet().stream().collect(toMap(Map.Entry::getKey, e -> e.getValue().size()));
         List<Map.Entry<Rank, Integer>> sortedRankOccurrences = sizeByRank.entrySet().stream()
                 .sorted(Comparator.comparingInt(Map.Entry::getValue))
                 .collect(toList());
@@ -85,66 +83,82 @@ class Hand implements Comparable<Hand> {
         }
 //        Streams.forEachPair();
 
+        HandValue handValue;
         if (flush.isPresent() && isConsecutive) {
-            return new Hand(STRAIGHT_FLUSH, hand); // highest in hand wins
+            handValue = STRAIGHT_FLUSH; // highest in hand wins
         } else if (maxOccurrence.getValue() == 4) {
-            return new Hand(FOUR_OF_A_KIND, hand); // highest in hand wins; if four of a kind on the board, then kicker
+            handValue = FOUR_OF_A_KIND; // highest in hand wins; if four of a kind on the board, then kicker
         } else if (maxOccurrence.getValue() == 3 && secondOccurrence.getValue() == 2) {
-            return new Hand(FULL_HOUSE, hand); // highest three wins, then highest pair, then kicker
+            handValue = FULL_HOUSE; // highest three wins, then highest pair, then kicker
         } else if (flush.isPresent()) {
-            return new Hand(FLUSH, hand); // highest non-equal rank card wins (even if it's the lowest in the flush)
+            handValue = FLUSH; // highest non-equal rank card wins (even if it's the lowest in the flush)
         } else if (isConsecutive) {
-            return new Hand(STRAIGHT, hand); // highest in hand wins, straight on board - split pot unless
+            handValue = STRAIGHT; // highest in hand wins, straight on board - split pot unless
         } else if (maxOccurrence.getValue() == 3) {
-            return new Hand(THREE_OF_A_KIND, hand); // highest in hand wins
+            handValue = THREE_OF_A_KIND; // highest in hand wins
         } else if (maxOccurrence.getValue() == 2 && secondOccurrence.getValue() == 2) {
-            return new Hand(TWO_PAIR, hand); // highest pair wins, then highest second pair, then kicker
+            handValue = TWO_PAIR; // highest pair wins, then highest second pair, then kicker
         } else if (maxOccurrence.getValue() == 2) {
-            return new Hand(ONE_PAIR, hand); // highest in hand, if equal then kicker
+            handValue = ONE_PAIR; // highest in hand, if equal then kicker
         } else {
-            return new Hand(HIGH_CARD, hand); // highest in hand, check next card
+            handValue = HIGH_CARD; // highest in hand, check next card
         }
+        return new Hand(handValue, hand);
     }
 
     @Override
     public int compareTo(Hand o) {
-        if (this.handValue.equals(o.handValue)) {
 
-            if (FULL_HOUSE.equals(handValue)) {
-                Map<Rank, List<Card>> byRank = cards.stream().collect(groupingBy(c -> c.rank));
-                Map<Rank, List<Card>> byRankOther = o.cards.stream().collect(groupingBy(c -> c.rank));
+        boolean handsValuesAreNotEqual = !this.handValue.equals(o.handValue);
 
-                // compare three of a kind
-                List<Map.Entry<Rank, List<Card>>> threes = byRank.entrySet().stream().filter(e -> e.getValue().size() == 3).collect(toList());
-                List<Map.Entry<Rank, List<Card>>> threesOther = byRankOther.entrySet().stream().filter(e -> e.getValue().size() == 3).collect(toList());
-                Rank threesRank = threes.get(0).getKey();
-                Rank threesRankOther = threesOther.get(0).getKey();
-                if (!threesRank.equals(threesRankOther)) {
-                    return threesRank.compareTo(threesRankOther);
-                }
-
-                // compare pair
-                List<Map.Entry<Rank, List<Card>>> pair = byRank.entrySet().stream().filter(e -> e.getValue().size() == 2).collect(toList());
-                List<Map.Entry<Rank, List<Card>>> pairOther = byRankOther.entrySet().stream().filter(e -> e.getValue().size() == 2).collect(toList());
-                Rank pairRank = pair.get(0).getKey();
-                Rank pairRankOther = pairOther.get(0).getKey();
-                return pairRank.compareTo(pairRankOther);
-            }
-
-            // assumes cards are sorted in reverse order
-            // compare highest ranks in hand, individually
-            Iterator<Card> otherator = o.cards.iterator();
-            for (Card highCard : this.cards) {
-                Card otherHighCard = otherator.next();
-                if (!highCard.rank.equals(otherHighCard.rank)) {
-                    return highCard.rank.compareTo(otherHighCard.rank);
-                } // else continue
-            }
-
-            return 0; // then they must be equal
+        if (handsValuesAreNotEqual) {
+            return compareByHandValue(o);
         } else {
-            return HAND_VALUE_COMPARATOR.compare(this.handValue, o.handValue);
+            return compareByRank(o);
         }
+    }
+
+    private int compareByHandValue(Hand o) {
+        return HAND_VALUE_COMPARATOR.compare(this.handValue, o.handValue);
+    }
+
+    private int compareByRank(Hand o) {
+        if (FULL_HOUSE.equals(handValue)) {
+            return compareFullHouse(o);
+        }
+
+        // assumes cards are sorted in reverse order
+        // compare highest ranks in hand, individually
+        Iterator<Card> otherator = o.cards.iterator();
+        for (Card highCard : this.cards) {
+            Card otherHighCard = otherator.next();
+            if (!highCard.rank.equals(otherHighCard.rank)) {
+                return highCard.rank.compareTo(otherHighCard.rank);
+            } // else continue
+        }
+
+        return 0; // then they must be equal
+    }
+
+    private int compareFullHouse(Hand o) {
+        Map<Rank, List<Card>> byRank = cards.stream().collect(groupingBy(c -> c.rank));
+        Map<Rank, List<Card>> byRankOther = o.cards.stream().collect(groupingBy(c -> c.rank));
+
+        // compare three of a kind
+        List<Map.Entry<Rank, List<Card>>> threes = byRank.entrySet().stream().filter(e -> e.getValue().size() == 3).collect(toList());
+        List<Map.Entry<Rank, List<Card>>> threesOther = byRankOther.entrySet().stream().filter(e -> e.getValue().size() == 3).collect(toList());
+        Rank threesRank = threes.get(0).getKey();
+        Rank threesRankOther = threesOther.get(0).getKey();
+        if (!threesRank.equals(threesRankOther)) {
+            return threesRank.compareTo(threesRankOther);
+        }
+
+        // compare pair
+        List<Map.Entry<Rank, List<Card>>> pair = byRank.entrySet().stream().filter(e -> e.getValue().size() == 2).collect(toList());
+        List<Map.Entry<Rank, List<Card>>> pairOther = byRankOther.entrySet().stream().filter(e -> e.getValue().size() == 2).collect(toList());
+        Rank pairRank = pair.get(0).getKey();
+        Rank pairRankOther = pairOther.get(0).getKey();
+        return pairRank.compareTo(pairRankOther);
     }
 
     @Override
